6402bbeca00aac856a8edbb239b6c574cb412a43
tsubhojit <53476779+tsubhojit@users.noreply.github.com>
2021-09-01 12:45:08 +0000

    updated connection string

--- pos.py
+++ pos.py
@@ -27,7 +27,7 @@ MONGODB_USER = "admin"
 MONGODB_PASSWORD = "admin"
 MONGODB_DATABASE = "Edge_BankDB"
 #MONGODB_DOMAIN = "45a49dbf-us-east.lb.appdomain.cloud"
-MONGODB_DOMAIN = "1e1b849f-eu-de.lb.appdomain.cloud"
+MONGODB_DOMAIN = "edgedb.edge-open-banking-tcs.svc"
 MONGODB_PORT = "27017"
 
 try:
